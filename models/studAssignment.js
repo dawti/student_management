5454765e3fbc8a259539f679a6c9881448970f8b
Arsheena <aazmi600@gmail.com>
2021-08-06 14:47:26 +0530

    assignment num removed

--- models/studAssignment.js
+++ models/studAssignment.js
@@ -1,9 +1,6 @@
 //arsheena student assignment
 module.exports = (sequelize, Sequelize) => {
 	const StudAssignment = sequelize.define('stud_assign', {
-      assignment_no: {
-        type: Sequelize.INTEGER
-      },
       assignment: {
         type: Sequelize.STRING 
       }
