4b27fd7d50a18534af1c51dd0a1e32d3f8181b2f
dawti <dawtiej@gmail.com>
2021-08-06 11:13:16 +0530

    class model

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express=require('express');
 const app=express();
 const port=8081;
 const Sequelize=require('sequelize');
-const sequelize=new Sequelize('postgres://postgres:root@localhost:5432/stud_db');
+const sequelize=new Sequelize('postgres://postgres:1234@localhost:5432/stud_db');
 
+const Class = require('./models/class.js')(sequelize, Sequelize);
 const Teacher = require('./models/teacher.js')(sequelize, Sequelize);
 const Course = require('./models/course.js')(sequelize, Sequelize);
-
 const Fee = require('./models/fee.js')(sequelize, Sequelize);
+
 
 sequelize.sync({force:true}).then(() => {
     console.log('Drop and Resync with { force: true }');
